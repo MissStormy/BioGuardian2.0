c543ba9897e62ee081b30717b0d754056d992f8a
Lara RG <laragoblin@outlook.com>
2023-10-10 10:55:02 +0200

    montado (parado)

--- src/main/java/bioguardian/bioguardian/AppCtrl.java
+++ src/main/java/bioguardian/bioguardian/AppCtrl.java
@@ -3,7 +3,7 @@ package bioguardian.bioguardian;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class AppCtrl {
     @FXML
     private Label welcomeText;
 
